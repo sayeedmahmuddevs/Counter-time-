486d43a6c96940e087ebe421be29254aacbadad4
Sayeed Mahmud <sayeedmahmud343@gmail.com>
2026-01-17 16:13:11 +0600

    logic change while update number

--- script.js
+++ script.js
@@ -7,52 +7,79 @@ const start = document.querySelector('.start');
 const stop = document.querySelector('.stop');
 const clear = document.querySelector('.clear');
 
-// timer counter
-    let hour = 0;
-    let munite = 0;
-    let second = 0;
-    let minsnd = 0
-    let interval;
+// timer varient
+const timer = {
+    hour: 0,
+    minute: 0,
+    second: 0,
+    milli: 0,
+    interval: null
+}
 
 // time Start
 
-function startTimer(){
-    if(interval) return;
-    interval = setInterval(()=>{
-        minsnd++
-        if(minsnd===99){
-            minsnd = 0
-            second++
-            if(second===60){
-                second = 0
-                munite++
-                if(munite===60){
-                    munite = 0;
-                    hour++
-                }
+function randerTime(){
+    const MILL_LIMIT = 100;
+    const SEC_LIMIT = 60;
+    const MIN_LIMIT = 60;
+    const INTERVAL = 10
+
+        if(timer.interval)return;
+
+        timer.interval = setInterval(() => {
+            timer.milli++
+
+            if(timer.milli === MILL_LIMIT){
+                timer.milli = 0;
+                timer.second++
             }
-        }
-        seconds.textContent = second<10 ? '0'+second : second
-        minSeconds.textContent = minsnd<10 ? '0'+minsnd : minsnd;
-        hours.textContent = hour<10 ? '0'+hour : hour;
-        munites.textContent = munite<10 ? '0'+munite : munite;
 
-    },10)
-    
+            if(timer.second === SEC_LIMIT){
+                timer.second = 0;
+                timer.minute++;
+                
+            }
+
+            if(timer.minute === MIN_LIMIT){
+                timer.minute = 0;
+                timer.hour++
+            }
+
+        uiUpdate()
+
+        }, INTERVAL)
+
 }
-      
+
+// the time variable number is 2 digit 
+function twoNumb(time){
+    return time < 10 ? '0' + time : time
+}
+
+// ui time update
+function uiUpdate(){
+
+    seconds.textContent = twoNumb(timer.second)
+    munites.textContent = twoNumb(timer.minute)
+    hours.textContent = twoNumb(timer.hour)
+    minSeconds.textContent = twoNumb(timer.milli)
+
+}
+
+// To stop time while it is running
 function stopTimer(){
-    clearInterval(interval)
-    interval = null
+    clearInterval(timer.interval)
+    timer.interval = null
 
 }
 
+//  To clear time while it is running
 function clearTimer(){
     stopTimer()
-    hour = 0
-    munite = 0
-    second = 0
-    minsnd = 0
+    timer.hour = 0
+    timer.minute = 0
+    timer.second = 0
+    timer.milli = 0
     seconds.textContent = '00'
     minSeconds.textContent = '00' 
     hours.textContent = '00'
@@ -60,7 +87,7 @@ function clearTimer(){
 
 }
 
-
-start.addEventListener('click', startTimer)
+// button click render
+start.addEventListener('click', randerTime)
 stop.addEventListener('click', stopTimer)
 clear.addEventListener('click', clearTimer)
